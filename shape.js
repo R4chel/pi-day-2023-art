500e4eec19d9777b998d3347e2bc1a1fa4d8bb64
R4chel <rae84@cornell.edu>
2023-03-14 12:29:59 -0400

    its a start

---
+++ shape.js
@@ -0,0 +1,21 @@
+function Shape({
+    r,
+    x,
+    y,
+    c
+}) {
+    this.r = r;
+    this.x = x;
+    this.y = y;
+    this.colors = c;
+    this.thetas = c.map((x) => random(0, 2 * PI)).sort();
+
+    this.draw = function() {
+        for (let i = 0; i < this.colors.length; i++) {
+            fill(this.colors[i]);
+
+            arc(this.x, this.y, this.r, this.r, this.thetas[i], this.thetas[i + 1 % this.thetas.length], PIE);
+
+        }
+    }
+}
